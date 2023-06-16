0426f562baa27775ed4d83ec5201d0aec02e7d62
Akshit Srivastava <88588444+restlesshornet@users.noreply.github.com>
2023-06-16 17:53:54 +0530

    Update app.py

--- src/Deployment/app.py
+++ src/Deployment/app.py
@@ -15,7 +15,7 @@ from ultralytics.yolo.utils.plotting import Annotator
 from cv2 import cvtColor
 import os
 
-
+#Importing the model
 
 model = YOLO('best.pt')
 def bgr2rgb(image):
@@ -76,6 +76,13 @@ def main():
         
     st.title("AI Road Inspection System")
     Header = st.container()
+    js_code = """
+        const elements = window.parent.document.getElementsByTagName('footer');
+        elements[0].innerHTML = "Mexico x Omdena VIT Bhopal Local Chapter " + new Date().getFullYear();
+        """
+    st.markdown(f"<script>{js_code}</script>", unsafe_allow_html=True)
+            
+    st.image("logo.png")
     
     ##MainMenu
     
@@ -95,7 +102,7 @@ def main():
     ##HOME page 
     
     if selected == "Project Information":
-        st.image("logo.png")
+        
         st.subheader("Problem Statement")
         problem_statement = """
         Current practices of performing road inspections are time-consuming and labour-intensive. Road surfaces degrade on a 
@@ -103,7 +110,12 @@ def main():
         impact economic efficiency. To maintain roads as efficiently as possible, municipalities perform regular
         inspections. The aim of the project is to use machine learning to study and analyze different types of road defects
         and to automatically detect any road abnormalities.
+        """
         
+        st.text(problem_statement)
+        
+        with st.expander("Read More"): 
+            text = """
         The goal of this project is to design, build and test an inspection system for detecting road abnormalities, defects, and damages
         using machine learning. The proposed system aims to improve the efficiency of road inspections and reduce
         the time and labor required for the process. The system will be equipped with a camera to capture video streams
@@ -112,10 +124,10 @@ def main():
         The approach will involve three main tasks: data acquisition, data training/testing, and dashboard building and testing. 
         Ultimately, the proposed system will help to maintain roads more efficiently, enhance driver comfort, and improve economic 
         efficiency. Additionally, the system will provide insights into the causes of road abnormalities in Indian roads, 
-        including pitfalls, sinks, flooding, and traffic congestion due to insufficient lanes in cities and towns.
-        """
+        including pitfalls, sinks, flooding, and traffic congestion due to insufficient lanes in cities and towns."""
+            
+            st.text(text)
         
-        st.text(problem_statement)
         st.subheader("Our Solution")
         Project_goal = """
         Our Team developed a Machine Learning ( ML ) model based on the YOLOv8 Architecture, which was trained on a comprehensive
